1b70e4770daa0547a49794e9323cd321b3176e86
Cahsun147 <suncah147@gmail.com>
2026-01-10 14:02:50 +0700

    frame alert kegedaan

--- app/docs/PageDocs.tsx
+++ app/docs/PageDocs.tsx
@@ -13,7 +13,7 @@ const PageDocs = (): JSX.Element => {
       <BleepsOnAnimator<BleepNames> transitions={{ entering: 'intro' }} continuous />
 
       <div className="flex-1 flex justify-center items-center p-4">
-        <div className="relative flex px-6 py-12 md:p-12 xl:p-20">
+        <div className="relative flex w-full max-w-3xl px-6 py-12 md:p-12 xl:p-20 overflow-hidden sm:overflow-visible">
           <Animator duration={{ enter: 0.6 }}>
             <FrameAlert />
           </Animator>
@@ -21,7 +21,7 @@ const PageDocs = (): JSX.Element => {
           <main className="relative flex flex-col items-center gap-6">
             <Animator duration={{ delay: 0.3 }}>
               <Animated
-                className="text-primary-4 text-[4rem]"
+                className="text-primary-4 text-[3rem] sm:text-[4rem]"
                 animated={['flicker', ['y', theme.space(4), 0]]}
               >
                 <IconDocumentation />
@@ -46,26 +46,26 @@ const PageDocs = (): JSX.Element => {
 
             <Animator duration={{ delay: 0.3 }}>
               <Animated
-                className="flex flex-row items-center gap-4"
+                className="flex flex-col sm:flex-row flex-wrap justify-center items-stretch sm:items-center gap-3 sm:gap-4 w-full"
                 animated={['flicker', ['y', theme.space(-6), 0]]}
               >
-                <Link href="/docs/developer">
-                  <Button tabIndex={-1}>
+                <Link href="/docs/developer" className="w-full sm:w-auto">
+                  <Button tabIndex={-1} className="w-full sm:w-auto text-size-11 sm:text-size-10">
                     For Developer <IconDocs />
                   </Button>
                 </Link>
-                <Link href="/docs/xgate-tutorial">
-                  <Button tabIndex={-1}>
+                <Link href="/docs/xgate-tutorial" className="w-full sm:w-auto">
+                  <Button tabIndex={-1} className="w-full sm:w-auto text-size-11 sm:text-size-10">
                     XGATE Tutorial <IconDocs />
                   </Button>
                 </Link>
-                <Link href="/docs/community">
-                  <Button tabIndex={-1}>
+                <Link href="/docs/community" className="w-full sm:w-auto">
+                  <Button tabIndex={-1} className="w-full sm:w-auto text-size-11 sm:text-size-10">
                     Community <IconDocs />
                   </Button>
                 </Link>
-                <Link href="/">
-                  <Button tabIndex={-1}>
+                <Link href="/" className="w-full sm:w-auto">
+                  <Button tabIndex={-1} className="w-full sm:w-auto text-size-11 sm:text-size-10">
                     Home <IconDocs />
                   </Button>
                 </Link>
