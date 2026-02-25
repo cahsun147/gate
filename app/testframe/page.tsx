21a6408b26aeb6c6c5970b35fdf476765f744381
Cahsun147 <suncah147@gmail.com>
2026-02-25 09:01:04 +0700

    coba rubah ukuran frame GateOmniTermina

--- app/testframe/page.tsx
+++ app/testframe/page.tsx
@@ -57,11 +57,11 @@ const PageTestFrame = (): JSX.Element => {
                 <FrameGateFolderTabGlassAssembler />
               </div>
 
-              <div className="grid grid-cols-2 gap-4 md:col-span-2">
-                <div className="relative" style={{ width: 500, height: 350 }}>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:col-span-2">
+                <div className="relative w-full h-[350px]">
                   <GateOmniTerminal />
                 </div>
-                <div className="relative" style={{ width: 500, height: 350 }}>
+                <div className="relative w-full h-[350px]">
                   <GateOmniTerminalAssembler />
                 </div>
               </div>
